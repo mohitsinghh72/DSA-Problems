9f70a8e9c7031d2bae8a597235242a373131341f
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-28 21:30:21 +0530

    Stack Implimentation

---
+++ Stack.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Practice stack = new Practice(5);
+
+        stack.push(10);
+        stack.push(11);
+        stack.push(12);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
+class Stack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int ptr = -1;
+
+    public Practice(){
+        this(DEFAULT_SIZE);
+    }
+    public Practice(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean push(int item){
+        if(isFull()){
+            System.out.println("Stack is full");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Cannot pop from Empty Stack");
+        }
+        int removed = data[ptr];
+        ptr--;
+        return removed;
+    }
+
+    public int peek() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Cannot peek from Empty Stack");
+        }
+        return data[ptr];
+    }
+    public boolean isFull(){
+        return ptr == data.length-1;
+    }
+
+    public boolean isEmpty(){
+        return ptr == -1;
+    }
+}
