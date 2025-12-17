d88b14ea034d567b7a5c559fb10a95e71bb03b01
Mohit Singh <mohitsinghh72@gmail.com>
2025-12-18 01:41:27 +0530

    Infinite Array Problem

---
+++ InfiniteArray.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
+        int target = 9;
+
+        int sol = ans(arr,target);
+        System.out.println(sol);
+
+
+    }
+    public static int ans(int[] arr,int target){
+        int start = 0;
+        int end = 1;
+
+        while(target > arr[end]){
+            int temp = end+1;
+            end = end + (end-start+1)*2;
+            start = temp;
+        }
+        return search(arr,target,start,end);
+    }
+    public static int search(int[] arr,int target,int start,int end){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target>arr[mid]){
+                start = mid+1;
+            }else if(target<arr[mid]){
+                end = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
